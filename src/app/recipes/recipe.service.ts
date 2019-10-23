5f1464e36b6c69b3017d6e784069f079a5422299
Timothy ALCAIDE <timothy@MacBook-Pro-de-Timothy.local>
2019-10-23 10:03:59 +0200

    feat(recipe): add ingredients

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
-import { AppModule } from '../app.module';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,16 +9,19 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
     new Recipe(
-      'A test recipe',
-      'This is a simply test',
-      'https://picsum.photos/id/1/200'
+      'Tasty Shnitzel',
+      'A super tasty Schnitzel, just awesome !',
+      'https://picsum.photos/id/1/200',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
     ),
     new Recipe(
-      'An other test recipe',
-      'This is a simply test',
-      'https://picsum.photos/id/2/200'
+      'Biggy Fat burger',
+      'What else ?',
+      'https://picsum.photos/id/2/200',
+      [new Ingredient('Meat', 1), new Ingredient('Buns', 2)]
     )
   ];
+
   getRecipes() {
     return this.recipes.slice();
   }
