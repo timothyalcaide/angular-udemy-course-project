79621e706e45c1bd7215d57a15d08674981fc00c
Timothy ALCAIDE <timothy@MacBook-Pro-de-Timothy.local>
2019-10-28 11:04:18 +0100

    feature(shopping): we can delete an ingredient

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -51,6 +51,11 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.editMode = false;
   }
 
+  onDelete() {
+    this.onClear();
+    this.slService.deleteIngredient(this.editedItemIndex);
+  }
+
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
