f8b9c8086c1381d5bd2487a5dce6c6a6b75187d3
Timothy ALCAIDE <timothy@MacBook-Pro-de-Timothy.local>
2019-10-30 11:08:10 +0100

    fix(recipe): add resolver to prevent the loading data that is don't fetch

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
 
@@ -21,20 +21,18 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http
-      .get<Recipe[]>(this.URL_API + 'recipes.json')
-      .pipe(
-        map(recipes => {
-          return recipes.map(recipe => {
-            return {
-              ...recipe,
-              ingredients: recipe.ingredients ? recipe.ingredients : []
-            };
-          });
-        })
-      )
-      .subscribe(recipes => {
+    return this.http.get<Recipe[]>(this.URL_API + 'recipes.json').pipe(
+      map(recipes => {
+        return recipes.map(recipe => {
+          return {
+            ...recipe,
+            ingredients: recipe.ingredients ? recipe.ingredients : []
+          };
+        });
+      }),
+      tap(recipes => {
         this.recipeService.setRecipes(recipes);
-      });
+      })
+    );
   }
 }
