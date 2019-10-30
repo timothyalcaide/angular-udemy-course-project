6f2471db7f72c94f97a8e11d2bd0c6dad0fe59e6
Timothy ALCAIDE <timothy@MacBook-Pro-de-Timothy.local>
2019-10-30 10:33:01 +0100

    fix(recipe): recipe without ingredients saved can be store in the database

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -11,20 +11,21 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Tasty Shnitzel',
-      'A super tasty Schnitzel, just awesome !',
-      'https://picsum.photos/id/1/200',
-      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
-    ),
-    new Recipe(
-      'Biggy Fat burger',
-      'What else ? Maybe some fried oignons ?',
-      'https://picsum.photos/id/2/200',
-      [new Ingredient('Meat', 1), new Ingredient('Buns', 2)]
-    )
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Tasty Shnitzel',
+  //     'A super tasty Schnitzel, just awesome !',
+  //     'https://picsum.photos/id/1/200',
+  //     [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
+  //   ),
+  //   new Recipe(
+  //     'Biggy Fat burger',
+  //     'What else ? Maybe some fried oignons ?',
+  //     'https://picsum.photos/id/2/200',
+  //     [new Ingredient('Meat', 1), new Ingredient('Buns', 2)]
+  //   )
+  // ];
+  private recipes: Recipe[] = [];
 
   constructor(private slService: ShoppingListService) {}
 
