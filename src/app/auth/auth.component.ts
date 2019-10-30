cc055b185bd907720054720a8faacb057fa272de
Timothy ALCAIDE <timothy@MacBook-Pro-de-Timothy.local>
2019-10-30 14:29:50 +0100

    feat(auth): add an auth section

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css']
+})
+export class AuthComponent implements OnInit {
+  isLogginMode = true;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onSwitchMode() {
+    this.isLogginMode = !this.isLogginMode;
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+    form.reset();
+  }
+}
