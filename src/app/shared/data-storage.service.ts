ea2f72a55af3b14b25600847576bdc561d34c988
Timothy ALCAIDE <timothy@MacBook-Pro-de-Timothy.local>
2019-10-30 10:33:01 +0100

    fix(recipe): recipe without ingredients saved can be store in the database

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
 
@@ -22,6 +23,16 @@ export class DataStorageService {
   fetchRecipes() {
     this.http
       .get<Recipe[]>(this.URL_API + 'recipes.json')
+      .pipe(
+        map(recipes => {
+          return recipes.map(recipe => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : []
+            };
+          });
+        })
+      )
       .subscribe(recipes => {
         this.recipeService.setRecipes(recipes);
       });
