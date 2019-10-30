a39714edda66446def5a25d7d2dff1d504e4680c
Timothy ALCAIDE <timothy@MacBook-Pro-de-Timothy.local>
2019-10-30 10:16:45 +0100

    feat(recipe): add get request to store recipes to the firebase database

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,4 @@
-import {
-  Component,
-  OnInit,
-  EventEmitter,
-  Output,
-  OnDestroy
-} from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 import { Router, ActivatedRoute } from '@angular/router';
