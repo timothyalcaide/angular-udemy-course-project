d9160f0e10998406a34859e6114b70e383ae54c9
Timothy ALCAIDE <timothy.alcaide@tabmo.io>
2019-10-31 14:50:08 +0100

    feat(auth): add a token attached with an interceptor

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,27 +1,32 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map, tap } from 'rxjs/operators';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { map, tap, take, exhaustMap } from 'rxjs/operators';
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataStorageService {
-  URL_API = 'https://ng-course-recipe-book-tabmo.firebaseio.com/';
-  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+  URL_BDD = 'https://ng-course-recipe-book-tabmo.firebaseio.com/';
+  constructor(
+    private http: HttpClient,
+    private recipeService: RecipeService,
+    private authService: AuthService
+  ) {}
 
   storeRecipes() {
     const recipes = this.recipeService.getRecipes();
     this.http
-      .put(this.URL_API + 'recipes.json', recipes)
+      .put(this.URL_BDD + 'recipes.json', recipes)
       .subscribe(response => {
         console.log(response);
       });
   }
 
   fetchRecipes() {
-    return this.http.get<Recipe[]>(this.URL_API + 'recipes.json').pipe(
+    return this.http.get<Recipe[]>(this.URL_BDD + 'recipes.json').pipe(
       map(recipes => {
         return recipes.map(recipe => {
           return {
