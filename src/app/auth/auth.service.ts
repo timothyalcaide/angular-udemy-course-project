86f91ecebe5d6afb520eb2c5f62960fda6568c60
Timothy ALCAIDE <timothy.alcaide@tabmo.io>
2019-10-31 15:30:00 +0100

    feat(auth): add autologging with local storage

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -77,6 +77,28 @@ export class AuthService {
     this.router.navigate(['/auth']);
   }
 
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+    if (!userData) {
+      return;
+    }
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate)
+    );
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+    }
+  }
+
   private handleAuthentification(
     email: string,
     userId: string,
@@ -86,6 +108,7 @@ export class AuthService {
     const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
     const user = new User(email, userId, token, expirationDate);
     this.user.next(user);
+    localStorage.setItem('userData', JSON.stringify(user));
   }
 
   private handleError(errorRes: HttpErrorResponse) {
